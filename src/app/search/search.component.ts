b9497353cea9c5dee7859545dbfac5c0670bbeb2
Lal <lalkiran.c@gmail.com>
2018-07-17 09:04:14 +0530

    nearest school commit

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,6 +11,7 @@ import {
   switchMap
 } from 'rxjs/operators';
 import { map, startWith } from 'rxjs/operators';
+import * as lodash from 'lodash';  
 
 @Component({
   selector: 'app-search',
@@ -54,6 +55,55 @@ export class SearchComponent implements OnInit {
       }
     });
     
+  }
+  
+  successFunction(position) {
+    lodash.chain(this.schools).sortBy('schoolName').map(function(school) {
+      if(this.isNearestSkool(position, school) > 10) {
+      }
+    }).value();
+
+  }
+  
+  isNearestSkool(position, option) {
+    let currentLat = position.coords.latitude;
+    let currentLng = position.coords.longitude;
+
+    let R = 6371; // Radius of the earth
+    
+    let latDistance = this.toRadians(currentLat - 10.068331);
+   
+    let lonDistance = this.toRadians(76.373784 - 76.383268);
+   
+    let a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(this.toRadians(10.068331))
+   
+               * Math.cos(this.toRadians(10.066091)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+   
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+   
+    let distance = R * c * 1000; // convert to meters
+   
+    distance = Math.pow(distance, 2) + Math.pow(0, 2);
+   
+    distance = Math.sqrt(distance);
+   
+    let distanceinnm = distance * 0.00054; // Meter to Nauticalmile
+  }
+
+  toRadians (angle) {
+    return angle * (Math.PI / 180);
+  }
+
+  errorFunction(){
+      alert("Geocoder failed");
+  }
+
+  searchNearestSkools(): void {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(this.successFunction, this.errorFunction);
+        } else {
+          this.schoolService.getAll().subscribe(schools => this.schools = schools);
+        }		
   }
 
   private _filter(value: string): String[] {
